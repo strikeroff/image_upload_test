65c47968e58eeb8ff78b63b083cdacf156f4c753
strikeroff <strikeroff@gmail.com>
2009-09-23 12:49:32 +0500

    delete migrations

--- db/migrate/20090804084608_add_upload_params_to_uploaded_file.rb
+++
@@ -1,9 +0,0 @@
-class AddUploadParamsToUploadedFile < ActiveRecord::Migration
-  def self.up
-    add_column :uploaded_files, :upload_params, :text
-  end
-
-  def self.down
-    remove_column :uploaded_files, :upload_params
-  end
-end
